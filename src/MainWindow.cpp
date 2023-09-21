0c8ad2cfe7b71869b3acb12e389e77473907b349
Yury Bobylev <bobilev_yury@mail.ru>
2023-09-21 18:05:36 +0400

    Orbits building cancel correction

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,19 +1,20 @@
 /*
- Copyright 2022-2023 Yury Bobylev <bobilev_yury@mail.ru>
-
- This file is part of EphEPM.
- EphEPM is free software: you can redistribute it and/or
- modify it under the terms of the GNU General Public License as
- published by the Free Software Foundation, either version 3 of
- the License, or (at your option) any later version.
- EphEPM is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with EphEPM. If not,
- see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2022-2023 Yury Bobylev <bobilev_yury@mail.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 #include "MainWindow.h"
 
@@ -57,7 +58,7 @@ MainWindow::MainWindow()
 
 MainWindow::~MainWindow()
 {
-  // TODO Auto-generated destructor stub
+
 }
 
 void
@@ -1707,10 +1708,11 @@ MainWindow::orbitsGraph(Gtk::Entry *day, Gtk::Entry *month, Gtk::Entry *year,
 	od->diagramPlot();
       });
 
-      canceled_disp->connect([win, od]
+      canceled_disp->connect([win, od, canceled_disp]
       {
 	win->close();
 	delete od;
+	delete canceled_disp;
       });
 
       od->calc_completed = [compl_disp]
@@ -1721,7 +1723,6 @@ MainWindow::orbitsGraph(Gtk::Entry *day, Gtk::Entry *month, Gtk::Entry *year,
       od->canceled_signal = [canceled_disp, compl_disp]
       {
 	canceled_disp->emit();
-	delete canceled_disp;
 	delete compl_disp;
       };
 
@@ -1753,7 +1754,7 @@ MainWindow::aboutProg()
   aboutd->set_application(this->get_application());
   aboutd->set_name("mainWindow");
   aboutd->set_program_name("EphEPM");
-  aboutd->set_version("1.0.1");
+  aboutd->set_version("1.0.2");
   aboutd->set_copyright("Copyright 2022 Yury Bobylev <bobilev_yury@mail.ru>");
   AuxFunc af;
   std::filesystem::path p = std::filesystem::u8path(af.get_selfpath());
